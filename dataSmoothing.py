c7517b96ced71cd199db43d41a1d32163c3dcdac
Rafael Beltran <rafabeltrans17@gmail.com>
2024-07-23 12:21:41 -0600

    Created smoother and detector

---
+++ dataSmoothing.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def dataSmoothing(previous_faces, faces, max_faces_to_track, min_detection_stability):
+    previous_faces.append(faces)
+    if len(previous_faces) > max_faces_to_track:
+        previous_faces.pop(0)
+
+    stable_faces = []
+    for face in faces:
+        if sum(1 for frame_faces in previous_faces 
+                if any(np.linalg.norm(np.array(face[:2]) - np.array(f[:2])) < 20 for f in frame_faces)
+            ) >= min_detection_stability:
+            stable_faces.append(face)
+
+    return stable_faces
+
+
---
+++ detector.py
@@ -0,0 +1,49 @@
+import cv2
+import numpy as np
+from camera import get_camera
+from dataSmoothing import dataSmoothing
+
+def detector():
+    camera = get_camera()
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+    camera.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
+    camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
+    
+
+    # Parametros para el smoothing de datos de caras detectadas
+    previous_faces = []
+    max_faces_to_track = 5
+    min_detection_stability = 5
+
+    while True:
+        ret, frame = camera.read()
+
+        if not ret:
+            break
+
+        frame_2_gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+
+        faces = face_cascade.detectMultiScale(frame_2_gray, 
+                                              scaleFactor=1.2, 
+                                              minNeighbors=6, 
+                                              minSize=(30, 30),
+                                              maxSize=(200, 200),
+                                              )
+        face_detected = False
+        stable_faces = dataSmoothing(previous_faces, faces, max_faces_to_track, min_detection_stability)
+
+        for(x, y, w, h) in stable_faces:
+            face_detected = True
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+            print(face_detected)
+
+        if not face_detected:
+            print(False)
+
+        cv2.imshow('Frame', frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+        
+    camera.release()
+    cv2.destroyAllWindows()
